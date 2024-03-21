f915f040181ed17e75db2c291da106294d38e6c5
fred-gutierrez <fredsg22@hotmail.com>
2024-03-21 13:55:00 -0600

    Fixed the linuxbrew issue

--- install.sh
+++ install.sh
@@ -6,20 +6,20 @@ USER="$(whoami)"
 # Basic Installs
 sudo apt update
 
-sudo apt install nala
-
-sudo nala install git
+# sudo apt install nala
+#
+# sudo nala install git
 
 # Install homebrew - https://www.digitalocean.com/community/tutorials/how-to-install-and-use-homebrew-on-linux
-sudo apt install build-essential
+# sudo apt install build-essential
 
 curl -fsSL -o brewInstall.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 
 /bin/bash brewInstall.sh
 
 if command -v brew &>/dev/null; then
-	echo 'eval "$(/home/${USER}/.linuxbrew/bin/brew shellenv)"' >>/home/${USER}/.profile
-	eval "$(/home/${USER}/.linuxbrew/bin/brew shellenv)"
+	echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>/home/${USER}/.bashrc
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 	brew doctor
 else
