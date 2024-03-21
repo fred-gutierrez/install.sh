c8cd3cabe54785ad27dac610e1f8632deb57b5b6
fred-gutierrez <fredsg22@hotmail.com>
2024-03-20 23:14:21 -0600

    Added nvim and tmux install with it's config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Re-usable variables
+# Variables
 USER="$(whoami)"
 
 # Basic Installs
@@ -13,20 +13,39 @@ sudo nala install git
 # Install homebrew - https://www.digitalocean.com/community/tutorials/how-to-install-and-use-homebrew-on-linux
 sudo apt install build-essential
 
-# curl -fsSL -o brewInstall.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-#
-# /bin/bash brewInstall.sh
+curl -fsSL -o brewInstall.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+/bin/bash brewInstall.sh
 
 if command -v brew &>/dev/null; then
 	echo 'eval "$(/home/${USER}/.linuxbrew/bin/brew shellenv)"' >>/home/${USER}/.profile
 	eval "$(/home/${USER}/.linuxbrew/bin/brew shellenv)"
 
 	brew doctor
-	brew install node
-	brew install neovim
 else
 	echo "Homebrew installation failed"
 fi
 
-# Install Neovim with current config = LazyVim
-sudo
+# Install Neovim with current config (Lazyvim)
+if command -v brew &>/dev/null; then
+	brew install node
+	brew install neovim
+else
+	echo "Homebrew not available/installed to install node and neovim"
+fi
+
+git clone https://github.com/fred-gutierrez/lazyvim-myconfig ~/.config/nvim
+
+# Install tmux
+sudo nala install tmux
+
+git clone https://github.com/fred-gutierrez/tmux-myconfig ~/.config/tmux
+
+# Install Virtual Machine Manager and QEMU
+
+# Install Razer Genie (Optional)
+
+# Install Solaar (Optional)
+
+# Remove shiet libreoffice and install onlyoffice
+sudo nala remove --purge "libreoffice*"
