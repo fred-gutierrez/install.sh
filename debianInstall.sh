b72327aed50bb40b9215fb37a20d0795b07416a5
fred-gutierrez <fredsg222@gmail.com>
2024-08-26 18:44:56 -0600

    Added conditional installing based on the responses of the user to Screenkey, Redshift and Razer Genie installations

--- debianInstall.sh
+++ debianInstall.sh
@@ -12,6 +12,8 @@ sudo apt install nala
 
 sudo nala install git
 
+sudo nala install xclip # Copy from the terminal
+
 # Homebrew - https://www.digitalocean.com/community/tutorials/how-to-install-and-use-homebrew-on-linux
 sudo apt install build-essential
 
@@ -61,7 +63,11 @@ brew tap arl/arl
 brew install gitmux
 
 # ScreenKey (for detecting key input for Videos)
-sudo nala install screenkey
+read -p "Do you want to install Screenkey - For detecting key input for videos - (yes/no): " screenkey_choice
+
+if [[ "$screenkey_choice" == "yes" || "$screenkey_choice" == "y" ]]; then
+  sudo nala install screenkey
+fi
 
 # Virtual Machine Manager and QEMU
 sudo nala install qemu-kvm qemu-system qemu-utils python3 python3-pip libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y
@@ -81,12 +87,36 @@ sudo usermod -aG input $USER
 sudo usermod -aG disk $USER
 
 # Razer Genie (Optional) - IMPORTANT: The download links vary on every distro - Check this page: https://software.opensuse.org//download.html?project=hardware%3Arazer&package=razergenie
-# The current commands are suited to work for a Debian 12 distro
-# sudo gpasswd -a $USER plugdev
-# echo 'deb http://download.opensuse.org/repositories/hardware:/razer/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/hardware:razer.list
-# curl -fsSL https://download.opensuse.org/repositories/hardware:razer/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/hardware_razer.gpg >/dev/null
-# sudo apt update
-# sudo apt install razergenie
+read -p "Do you want to install Razer Genie (yes/no): " razergenie_choice
+
+if [[ "$razergenie_choice" == "yes" || "$razergenie_choice" == "y" ]]; then
+  sudo gpasswd -a $USER plugdev
+  echo "You can check which debian version you have by opening a new terminal and doing 'cat/etc/debian_version'"
+  read -p "Which distro would you like to download it for? (debian 11/debian 12/debian testing/debian unstable): " razergenie_distro_choice
+
+  if [[ "$razergenie_distro_choice" == "debian11" || "$razergenie_distro_choice" == "debian 11" ]]; then
+    echo 'deb http://download.opensuse.org/repositories/hardware:/razer/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/hardware:razer.list
+    curl -fsSL https://download.opensuse.org/repositories/hardware:razer/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/hardware_razer.gpg >/dev/null
+    sudo nala install razergenie
+
+  elif [[ "$razergenie_distro_choice" == "debian12" || "$razergenie_distro_choice" == "debian 12" ]]; then
+    echo 'deb http://download.opensuse.org/repositories/hardware:/razer/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/hardware:razer.list
+    curl -fsSL https://download.opensuse.org/repositories/hardware:razer/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/hardware_razer.gpg >/dev/null
+    sudo nala install razergenie
+
+  elif [[ "$razergenie_distro_choice" == "debiantesting" || "$razergenie_distro_choice" == "debian testing" ]]; then
+    echo 'deb http://download.opensuse.org/repositories/hardware:/razer/Debian_Testing/ /' | sudo tee /etc/apt/sources.list.d/hardware:razer.list
+    curl -fsSL https://download.opensuse.org/repositories/hardware:razer/Debian_Testing/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/hardware_razer.gpg >/dev/null
+    sudo nala install razergenie
+
+  elif [[ "$razergenie_distro_choice" == "debianunstable" || "$razergenie_distro_choice" == "debian unstable" ]]; then
+    echo 'deb http://download.opensuse.org/repositories/hardware:/razer/Debian_Unstable/ /' | sudo tee /etc/apt/sources.list.d/hardware:razer.list
+    curl -fsSL https://download.opensuse.org/repositories/hardware:razer/Debian_Unstable/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/hardware_razer.gpg >/dev/null
+    sudo nala install razergenie
+  fi
+else
+  echo "Razer Genie installation skipped."
+fi
 
 # Remove libreoffice (Onlyoffice is my preferred and can be installed by the software manager)
 echo ""
@@ -94,14 +124,17 @@ echo "This next script is to remove LibreOffice (Which the preferred is Only Off
 sleep 5
 sudo nala remove --purge "libreoffice*"
 
-# Redshift and set it automatically
-sudo nala install redshift redshift-gtk
+# Redshift (and automatically configured)
+read -p "Do you want to install Redshift (yes/no): " redshift_choice
 
-sudo apt-get install libxcb1-dev libxcb-randr0-dev libx11-dev
+if [[ "$redshift_choice" == 'yes' || "$redshift_choice" == 'y' ]]; then
+  sudo nala install redshift redshift-gtk
 
-touch ~/.config/redshift.conf #redshift.conf MUST be within the .config file and cannot be within any other folder
+  sudo apt-get install libxcb1-dev libxcb-randr0-dev libx11-dev
 
-cat <<EOL >~/.config/redshift.conf
+  touch ~/.config/redshift.conf #redshift.conf MUST be within the .config file and cannot be within any other folder
+
+  cat <<EOL >~/.config/redshift.conf
 [redshift]
 temp-day=4000
 temp-night=4000
@@ -117,28 +150,22 @@ lon=0
 [randr]
 screen=0
 EOL
+fi
 
 # Font manager
 sudo nala install font-manager
 
 # TLP | For laptops only
-install_tlp() {
-  echo "Installing TLP..."
-  sudo nala install tlp -y
-  echo "Starting TLP..."
-  sudo tlp start
-  echo "Configuring TLP for battery optimization..."
-  sudo tlp bat
-  echo "TLP installation and configuration completed."
-}
-
 read -p "Are you using a laptop? (yes/no): " laptop_choice
 
 if [[ "$laptop_choice" == "yes" || "$laptop_choice" == "y" ]]; then
   # Prompt the user if they want to install TLP
   read -p "Do you want to install TLP for power management? (yes/no): " tlp_choice
   if [[ "$tlp_choice" == "yes" || "$tlp_choice" == "y" ]]; then
-    install_tlp
+    sudo nala install tlp -y
+    sudo tlp start
+    sudo tlp bat
+    echo "TLP installation and configuration completed."
   else
     echo "TLP installation skipped."
     sleep 5
